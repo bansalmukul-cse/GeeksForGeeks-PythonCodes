01f00dad8c7ed0798d1f14576e3b893e67b19a85
Mukul Bansal <bansalmukul.cse@gmail.com>
2026-02-18 19:19:03 +0530

    Add The-Dice-Problem.py

---
+++ The-Dice-Problem.py
@@ -0,0 +1,8 @@
+#User function Template for python3
+
+class Solution:
+    def oppositeFaceOfDice(self, n):
+    	#code here 
+    	return 7-n
+    	#the opposite sum of 2 sides is 7 like 2 opposite is 5 =7
+

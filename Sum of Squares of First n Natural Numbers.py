98c795b3137b1597102596858d5494cf76460481
MUKUL BANSAL <curlvector747@gmail.com>
2026-03-03 17:09:59 +0530

    Create Sum of Squares of First n Natural Numbers.py

---
+++ Sum of Squares of First n Natural Numbers.py
@@ -0,0 +1,8 @@
+#User function Template for python3
+class Solution:
+    # Function to calculate the sum of squares of first 'number' natural numbers
+    def sumOfSquares(self, number):
+        sum=0;
+        for i in range(1,number+1):
+            sum = sum + i*i
+        return sum
